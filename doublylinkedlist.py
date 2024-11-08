9c8e3ff56b372e886735c457791d2f6100c05040
Maximilian Menne <menne.max05@gmail.com>
2024-11-08 02:51:26 +0100

    bug fixes and new doubly linked list

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -2,12 +2,13 @@ class Node:
     def __init__(self, value: any) -> None:
         self.value = value
         self.next = None
+        self.previous = None
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self) -> None:
         self.head = None
+        self.tail = None
 
-    # O(n) linear time
     def __repr__(self) -> str:
         if self.head is None:
             return '[]'
@@ -20,7 +21,6 @@ class LinkedList:
             return_string += ']'
             return return_string
 
-    # O(n) linear time
     def __contains__(self, value: any) -> bool:
         last = self.head
         while last is not None:
@@ -29,7 +29,6 @@ class LinkedList:
             last = last.next
         return False   
 
-    # O(n) linear time
     def __len__(self) -> int:
         last = self.head
         counter = 0
@@ -38,24 +37,26 @@ class LinkedList:
             last = last.next
         return counter
 
-    # O(n) linear time
     def append(self, value: any) -> None:
-        new_node = Node(value)
         if self.head is None:
-            self.head = new_node
+            self.head = Node(value)
+            self.tail = self.head
         else:
-            last = self.head
-            while last.next:
-                last = last.next
-            last.next = new_node
+            last_node = Node(value)
+            last_node.previous = self.tail
+            self.tail.next = last_node
+            self.tail = last_node
 
-    # O(1) constant time
     def prepend(self, value: any) -> None:
-        first_node = Node(value)
-        first_node.next = self.head
-        self.head = first_node
+        if self.head is None:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            first_node = Node(value)
+            first_node.next = self.head
+            self.head.previous = first_node
+            self.head = first_node
 
-    # O(n) linear time
     def insert(self, value: any, index: int) -> None:
         if index == 0:
             self.prepend(value)
@@ -68,19 +69,32 @@ class LinkedList:
                     if last.next is None:
                         raise ValueError('Index out of bounds')
                     last = last.next
+                
                 new_node = Node(value)
                 new_node.next = last.next
+                new_node.previous = last
+                if last.next is not None:
+                    last.next.previous = new_node
                 last.next = new_node
+                if new_node.next is None:
+                    self.tail = new_node
 
-    # O(n) linear time
     def delete(self, value: any) -> None:
         last = self.head
         if last is not None:
             if last.value == value:
                 self.head = last.next
+                if self.head:
+                    self.head.previous = None
+                else:
+                    self.tail = None
             else:
                 while last.next:
                     if last.next.value == value:
+                        if last.next == self.tail:
+                            self.tail = last
+                        if last.next.next is not None:
+                            last.next.next.previous = last
                         last.next = last.next.next
                         break
                     last = last.next
@@ -89,13 +103,16 @@ class LinkedList:
         else:
             raise ValueError('List is empty')
 
-    # O(n) linear time
     def pop(self, index: int) -> any:
         if self.head is None:
             raise ValueError('List is empty')
-        if index == 0:
+        elif index == 0:
             value = self.head.value
             self.head = self.head.next
+            if self.head:
+                self.head.previous = None
+            else:
+                self.tail = None
             return value
         else:
             last = self.head
@@ -106,19 +123,23 @@ class LinkedList:
             if last.next is None:
                 raise ValueError('Index out of bounds')
             value = last.next.value
+            if last.next.next is not None:
+                last.next.next.previous = last
             last.next = last.next.next
+            if last.next is None:
+                self.tail = last
             return value
 
-    # O(n) linear time
     def get(self, index: int) -> any:
         if self.head is None:
             raise ValueError('Index out of bounds')
-        last = self.head
-        for i in range(index):
-            if last.next is None:
-                raise ValueError('Index out of bounds')
-            last = last.next
-        return last.value
+        else:
+            last = self.head
+            for i in range(index):
+                if last.next is None:
+                    raise ValueError('Index out of bounds')
+                last = last.next
+            return last.value
 
 if __name__ == '__main__':
     pass
